68baa3c45cad3e0c7cf20319fd1e70e1c14105c1
mr8god <mr8god@hotmail.com>
2020-05-21 18:09:40 +0800

    this is a successful thread code

---
+++ hhh/mutexandvarible.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <pthread.h>
+#define MAX 1000000000
+pthread_mutex_t the_mutex;
+pthread_cond_t condc, condp;
+int buffer = 0;
+
+void *producer(void *ptr)
+{
+    int i;
+    for (i = 1; i <= MAX; i++){
+        pthread_mutex_lock(&the_mutex);
+        while (buffer!=0)
+        {
+            pthread_cond_wait(&condp, &the_mutex);
+        }
+        buffer = i;
+        pthread_cond_signal(&condc);
+        pthread_mutex_unlock(&the_mutex);        
+    }
+    pthread_exit(0);
+}
+
+void *consumer(void *ptr)
+{
+    int i;
+    for (i = 1; i <= MAX; i++){
+        pthread_mutex_lock(&the_mutex);
+        while (buffer == 0)
+        {
+            pthread_cond_wait(&condc, &the_mutex);
+        }
+        buffer = 0;
+        pthread_cond_signal(&condp);
+        pthread_mutex_unlock(&the_mutex);        
+    }
+    pthread_exit(0);
+}
+
+/* 这是一个能够成功运行的单个缓冲区下的生产者消费者线程代码
+    执行命令是：gcc mutexandvarible.c -o thread -lphtread
+    运行在Ubuntu下是可以的
+    */
+
+
+int main(int argc, char **argv)
+{
+    pthread_t pro, con;
+    pthread_mutex_init(&the_mutex, 0);
+    pthread_cond_init(&condc, 0);
+    pthread_cond_init(&condp, 0);
+    pthread_create(&con, 0, consumer, 0);
+    pthread_create(&pro, 0, producer, 0);
+    pthread_join(pro, 0);
+    pthread_join(con, 0);
+    pthread_cond_destroy(&condc);
+    pthread_cond_destroy(&condp);
+    pthread_mutex_destroy(&the_mutex);
+}
